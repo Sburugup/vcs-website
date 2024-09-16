014c40e3fccaba47db549052fff3925ea0861c7a
Roshan Raj <112398277+Roshan3580@users.noreply.github.com>
2024-09-15 22:02:00 -0700

    Updated a small typo

--- src/ExecBoard.js
+++ src/ExecBoard.js
@@ -2,22 +2,22 @@ import React from 'react';
   
   const ExecTeam = () => {
     const execMembers = [
-      { name: 'Mihika Guntur', position: 'President', imgSrc: './mihika.jpg' },
-      { name: 'Divija Mudumbai', position: 'Co-Exec Vice President', imgSrc: './divija.jpg' },
-      { name: 'Hussain Mahuvawala', position: 'Co-Exec Vice President', imgSrc: './hussain.jpg' },
-      { name: 'Ayaan Dhir', position: 'VP of Professional Development', imgSrc: './ayaan.jpg' },
-      { name: 'Meghana Burugupalli', position: 'VP of Technology', imgSrc: './meghana.jpg' },
-      { name: 'Christian Mccormick', position: 'VP of External Affairs', imgSrc: './christian.jpg' },
-      { name: 'Ritika Ramnani', position: 'Co-VP of Marketing', imgSrc: './ritika.jpg' },
-      { name: 'Sheryl Gupta', position: 'Co-VP of Marketing', imgSrc: './sheryl.jpg' },
-      { name: 'Sudaay Chaloo', position: 'VP of Internal Affairs', imgSrc: './sudaay.jpg' },
-      { name: 'Matt Ayabe', position: 'VP of Engagement', imgSrc: './matt.jpg' },
-      { name: 'Kyle King', position: 'VP of Finance', imgSrc: './kyle.jpg' }
+      { name: 'Mihika Guntur', position: 'President', image: '/mihika.png' },
+      { name: 'Divija Mudumbai', position: 'Co-Exec Vice President', image: './divija.png' },
+      { name: 'Hussain Mahuvawala', position: 'Co-Exec Vice President', image: './hussain.png' },
+      { name: 'Ayaan Dhir', position: 'VP of Professional Development', image: './ayaan.jpg' },
+      { name: 'Meghana Burugupalli', position: 'VP of Technology', image: './meghana.jpg' },
+      { name: 'Christian Mccormick', position: 'VP of External Affairs', image: './christian.png' },
+      { name: 'Ritika Ramnani', position: 'Co-VP of Marketing', image: './ritika.jpg' },
+      { name: 'Sheryl Gupta', position: 'Co-VP of Marketing', image: './sheryl.jpg' },
+      { name: 'Sudaay Chaloo', position: 'VP of Internal Affairs', image: './sudaay.jpeg' },
+      { name: 'Matt Ayabe', position: 'VP of Engagement', image: './matt.jpg' },
+      { name: 'Kyle King', position: 'VP of Finance', image: './kyle.jpg' }
     ];
   
     return (
       <div className="container mx-auto py-16">
-        <h1 className="text-4xl font-bold text-center mb-12">Executive Team</h1>
+        <h1 className="text-4xl font-bold text-center mb-12">Executive Board</h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           {execMembers.map((member, index) => (
             <div key={index} className="text-center">
