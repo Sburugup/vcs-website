77f57f0081d52f6364f2f6718da2b762ccbdc2fe
Jason Nguyen <jasonnguyen7506@gmail.com>
2025-09-06 22:50:20 -0700

    modules

---
+++ src/components/plans.js
@@ -0,0 +1,7 @@
+// make components for better readability
+
+// have a components for the calendar
+
+// have a components for the board members
+
+// have a components for the where we go / make a separate page, also have like board testimonials from skills developed for internships?
